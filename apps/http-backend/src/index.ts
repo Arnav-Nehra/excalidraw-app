0340eef56327b4f5ad876feed2b6306406534843
Arnav Nehra <arnavnehra@Arnavs-MacBook-Pro.local>
2025-04-15 22:06:38 +0530

    working on it

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,13 +1,33 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
 import { middleware } from "./middleware";
-
+import {JWT_SECRET} from 'backend-common/config'
+import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types";
 const app = express();
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", (req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+      res.json({
+        message:"incorrect inputs"
+      })
+      return;
+    }
+    res.json({
+      userId:"123"
+    })
+    }
+);
 
 app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message : "incorrect inputs"
+    })
+    return;
+  }
   const userId = 1;
   const token = jwt.sign(
     {
@@ -21,6 +41,11 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/room",middleware,(req,res)=>{
-     
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+      res.json({
+        message:"incorrect inputs"
+      })
+    }
 })
 app.listen(3000);
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -11,5 +11,5 @@ export const SigninSchema = z.object({
 })
 
 export const CreateRoomSchema = z.object({
-    name:z.string().min(3).max(20)
+    name:z.string().min(3).max(20),
 })
