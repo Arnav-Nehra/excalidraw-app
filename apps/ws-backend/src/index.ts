31a3e23fca485f67be7f0fc2c92a51cb2dfcfdfe
Arnav-Nehra <arnavnehra1@gmail.com>
2025-04-19 19:01:45 +0530

    websocket layer

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,18 @@
 import {WebSocketServer} from "ws";
-
+import jwt from 'jsonwebtoken'
+import {JWT_SECRET} from "@repo/backend-common/config"
 const wss = new WebSocketServer({port:8080})
+
+function checkUser(token:string): string | null {
+    const decoded = jwt.verify(token,JWT_SECRET)
+    if(typeof decoded == "string"){
+        return null;
+    }
+    if(!decoded || !decoded.UserId){
+        return null;
+    }
+    return decoded.UserId;
+}
 
 wss.on('connection',function connection(ws,request){
     const url = request.url;
@@ -9,6 +21,13 @@ wss.on('connection',function connection(ws,request){
     }
     const queryParams = new URLSearchParams(url.split('?')[1])
     const token = queryParams.get('token')|| "";
+
+    const userId = checkUser(token)
+    if(!userId){
+        ws.close();
+        return;
+    }
+
     ws.on('message',function message(data){
         ws.send("pong")
     })
